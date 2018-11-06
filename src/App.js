809d22bd51afcb388de710397b6b3022bfb0c780
Grace Casale <gracecasale@gmail.com>
2018-11-05 20:54:38 -0500

    created box, updated styling in CSS to center all objects and items, added buttons

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        It works!
+      <div>
+        <header className="App-header">
+          <h1 className="title">Is Today Your Birthday?</h1>
+        </header>
+        <main className="App-main">
+          <div className="box"></div>
+          <button className="yes-button">Yes</button>
+          <button className="no-button">No</button>
+        </main>
+        <footer className="App-footer">
+        Made by Grace Casale
+        </footer>
       </div>
     );
   }
