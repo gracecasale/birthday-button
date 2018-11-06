8054c1377908183b94df5412d245823cac2b65b1
Grace Casale <gracecasale@gmail.com>
2018-11-06 18:54:40 -0500

    added code for on click of buttons to say Happy Birthday or otherwise do nothing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+  super(props)
+  this.state = {
+    showText: false
+  };
+
+  this.onYesSelect = this.onYesSelect.bind(this);
+  this.onNoSelect = this.onNoSelect.bind(this);
+}
+
+onYesSelect(text, event) {
+  this.setState({
+    showText: true
+  })
+}
+
+onNoSelect(text, event) {
+  this.setState({
+    showText: false
+  })
+}
+
   render() {
     return (
       <div>
@@ -9,12 +31,13 @@ class App extends Component {
           <h1 className="title">Is Today Your Birthday?</h1>
         </header>
         <main className="App-main">
-          <div className="box"></div>
-          <button className="yes-button">Yes</button>
-          <button className="no-button">No</button>
+          <div className="box">{this.state.showText ? "Happy Birthday!" : ""}  </div>
+          <div className="buttons-row">
+          <button className="btn yes-button" onClick={this.onYesSelect}>Yes</button>
+          <button className="btn no-button" onClick={this.onNoSelect}>No</button>
+          </div>
         </main>
         <footer className="App-footer">
-        Made by Grace Casale
         </footer>
       </div>
     );
