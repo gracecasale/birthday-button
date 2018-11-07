e21b8f8b8c5173219b17afc25f7f36756d18c4ee
Grace Casale <gracecasale@gmail.com>
2018-11-06 19:31:33 -0500

    Updates

---
+++ precache-manifest.859918abede2e9cb81599e9fbd6b1000.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f522976b4022b61af8e",
+    "url": "/birthday-button/static/js/runtime~main.6f522976.js"
+  },
+  {
+    "revision": "219f962a1723d4e9821f",
+    "url": "/birthday-button/static/js/main.219f962a.chunk.js"
+  },
+  {
+    "revision": "7e417a777a4d86431b2a",
+    "url": "/birthday-button/static/js/1.7e417a77.chunk.js"
+  },
+  {
+    "revision": "219f962a1723d4e9821f",
+    "url": "/birthday-button/static/css/main.35053c76.chunk.css"
+  },
+  {
+    "revision": "e4b58a542089795b4638b71da3c0f610",
+    "url": "/birthday-button/index.html"
+  }
+];
